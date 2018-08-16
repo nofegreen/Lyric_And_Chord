9b6d093a175717b935a915ec01e9e58b55214fdb
NOFE GREEN <nofegreen@gmail.com>
2018-08-16 10:27:37 +0700

    Update Admob

--- app/src/main/java/com/nofegreen/chord/Home.java
+++ app/src/main/java/com/nofegreen/chord/Home.java
@@ -18,56 +18,16 @@ import com.google.android.gms.ads.AdRequest;
 public class Home extends AppCompatActivity {
 
     private FrameLayout lyricCard,shareCard,moreappCard;
-    private AdView mAdView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
 
-        // Sample AdMob app ID: ca-app-pub-3940256099942544/6300978111
-        mAdView = findViewById(R.id.adView);
-        AdRequest adRequest = new AdRequest.Builder().build();
-        mAdView.loadAd(adRequest);
-
-        MobileAds.initialize(this, "ca-app-pub-3940256099942544/6300978111");
-        AdView adView = new AdView(this);
-        adView.setAdSize(AdSize.BANNER);
-        adView.setAdUnitId("ca-app-pub-3940256099942544/6300978111");
-
         // definisi card
         lyricCard = (FrameLayout) findViewById(R.id.lyric_id);
         shareCard = (FrameLayout) findViewById(R.id.share_id);
         moreappCard = (FrameLayout) findViewById(R.id.moreapp_id);
-
-        mAdView.setAdListener(new AdListener() {
-            @Override
-            public void onAdLoaded() {
-                // Code to be executed when an ad finishes loading.
-            }
-
-            @Override
-            public void onAdFailedToLoad(int errorCode) {
-                // Code to be executed when an ad request fails.
-            }
-
-            @Override
-            public void onAdOpened() {
-                // Code to be executed when an ad opens an overlay that
-                // covers the screen.
-            }
-
-            @Override
-            public void onAdLeftApplication() {
-                // Code to be executed when the user has left the app.
-            }
-
-            @Override
-            public void onAdClosed() {
-                // Code to be executed when when the user is about to return
-                // to the app after tapping on an ad.
-            }
-        });
 
         //add click listener
         lyricCard.setOnClickListener(new View.OnClickListener() {
@@ -100,7 +60,7 @@ public class Home extends AppCompatActivity {
         mi.putExtra(Intent.EXTRA_TEXT, "https://play.google.com/store/apps/details?id=com.nofegreen.icon");
         mi.setType("text/plain");
         startActivity(mi);
-    };
+    }
 
     @Override
     public void onBackPressed() {
